72003a5b7f74b0f7fb46244f3bcabe20ab6d7a3e
s0569571 <nico.schultze90@web.de>
2020-12-04 17:32:42 +0100

    add the number generator

---
+++ generator.py
@@ -0,0 +1,20 @@
+print('This is just a simple programm')
+
+import random as rn
+
+
+while True:
+    rnd_num = rn.randint(1, 5)
+    guess = 5
+    if guess == rnd_num:
+        print('damn u slick - num: ' + str(rnd_num))
+        break
+    else:
+        print('u kinda dogshit - num: ' + str(rnd_num))
+        continue
+
+
+
+
+
+
